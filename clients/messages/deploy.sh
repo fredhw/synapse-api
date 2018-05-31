83a5036b80c4b84a80e95c3848c516b7e2ce82a7
Frederick Harrison Wijaya <fredhw@uw.edu>
2018-05-31 15:18:56 -0700

    deleted unused client

--- clients/messages/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-./build.sh
-
-docker push fredhw/messages
-
-ssh root@165.227.23.185 'bash -s' < run.sh
--- clients/summary/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-./build.sh
-
-docker push fredhw/summary
-
-ssh root@165.227.23.185 'bash -s' < run.sh
